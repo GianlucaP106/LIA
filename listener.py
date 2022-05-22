70ae4700102e93f1d1e03e4708f2bb12d1d4d89e
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-05-22 19:21:22 -0400

    format

--- listener.py
+++ listener.py
@@ -1,12 +1,15 @@
 import speech_recognition as sr
 
+class Listener:
+    def __init__(self):
+        self.r = sr.Recognizer()
+        self.audio = None
+    def listen(self):
+        with sr.Microphone() as source:
+            print("Say something!")
+            self.audio = self.r.listen(source)
+        with open("microphone-results.wav", "wb") as f:
+            f.write(self.audio.get_wav_data())
 
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-with open("microphone-results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
+    
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from listener import Listener
+
+if __name__ == '__main__':
+    listener = Listener()
+    listener.listen()
