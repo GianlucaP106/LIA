0d6b669d104341c7b5df10b6e465186f965a41c2
Matthew Cabral <matthew.larbac@gmail.com>
2022-05-24 19:53:51 -0400

    Setting up the frontend

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './styles/styles.scss';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import './index.css';
 import App from './routes/App';
 import Dashboard from './routes/Dashboard';
 import reportWebVitals from './reportWebVitals';
